5446ef0c1b3cb03cee074803b43326cb6222b5c8
unknown <moabi@psybergate.co.za>
2022-03-04 12:35:08 +0200

    'lastes'

--- contact.js
+++ contact.js
@@ -22,7 +22,7 @@ var email = document.getElementById("email").value;
 var phone = document.getElementById("phone").value;
 var message = document.getElementById("message").value;
 var error_message = document.getElementById("error_massage");
-error_message.style.padding ="10px";
+error_message.style.padding ="5px";
 var text;
 
 if(name.length<5){
@@ -66,9 +66,3 @@ footer();
   function footer(){
     document.getElementById('footertable').innerHTML=' <a href="https://www.facebook.com/moabi.sethojane.3/" target="blank"> <img id="facebookicon"src="facebook-social-media-icon-facebook-logo.png"> facebook</a></td> <a href="https://www.linkedin.com/in/moabi-sethojane-32346892/" target="blank"><img id="linkedinicon" src="Linkedin-Logo.png">  linkedin</a>   <a href="https://twitter.com/MoabiSethojane" target="blank"> <img id="twittericon" src="free-twitter-logo-icon-2429-thumb.png"> twitter</a> <img id="whatappicon" src="whatsapp-social-media-icon-design-template-vector-22339907.jpg"> whatsapp number:0685335569  <img id="phoneNumber" src="phone-icon-telephone-icon-which-i-going-to-present-you-unique-you-totally-different-any-other-icons-you-111739308.jpg"> contact number: 0614666063';
   }
-
-
-
-
-
-
